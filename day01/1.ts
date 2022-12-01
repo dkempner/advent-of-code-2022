cf6372a7ab9ec38988cb1a947499ec26d642c64d
Daniel Kempner <daniel.kempner@instacart.com>
2022-11-30 21:06:50 -0800

    day01 solved

--- day01/1.ts
+++ day01/1.ts
@@ -1,7 +1,16 @@
 import { solve, bitsToInt } from './helper.ts'
 
 solve((lines) => {
+  let result = 0
+  let mini = 0
   lines.forEach((line) => {
-    console.log(line)
+    if (!line) {
+      if (mini > result) {
+        result = mini
+      }
+      mini = 0
+    }
+    mini = mini + Number(line)
   })
+  return result
 })
--- day01/2.ts
+++ day01/2.ts
@@ -1,7 +1,18 @@
 import { solve, bitsToInt } from './helper.ts'
 
 solve((lines) => {
+  let result = 0
+  let mini = 0
+  let all: number[] = []
   lines.forEach((line) => {
-    console.log(line)
+    if (!line) {
+      all.push(mini)
+      mini = 0
+    }
+    mini = mini + Number(line)
   })
+  return all
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((a, b) => a + b, 0)
 })
