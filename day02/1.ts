6f393e3fca0d4bb6bb1b0b5081f134430b5e9809
Daniel Kempner <daniel.kempner@instacart.com>
2022-12-02 08:38:35 -0800

    day02 solved

--- day02/1.ts
+++ day02/1.ts
@@ -1,7 +1,25 @@
 import { solve, bitsToInt, lodash } from './helper.ts'
 
+const points: Record<string, Record<string, number>> = {
+  A: { X: 3, Y: 6, Z: 0 },
+  B: { X: 0, Y: 3, Z: 6 },
+  C: { X: 6, Y: 0, Z: 3 },
+}
+
+const points2: Record<string, number> = {
+  X: 1,
+  Y: 2,
+  Z: 3,
+}
+
 solve((lines) => {
   let result = 0
-  lines.forEach((line) => {})
+  lines.forEach((line) => {
+    const chars = line.split('')
+    const them = line[0]
+    const me = line[2]
+
+    result = result + points2[me] + points[them][me]
+  })
   return result
 })
--- day02/2.ts
+++ day02/2.ts
@@ -1,7 +1,39 @@
 import { solve, bitsToInt, lodash } from './helper.ts'
 
+// X - i need to Lose,
+// Y - i need to Draw,
+// Z - i need to Win
+
+const points: Record<string, Record<string, string>> = {
+  A: { X: 'Z', Y: 'X', Z: 'Y' },
+  B: { X: 'X', Y: 'Y', Z: 'Z' },
+  C: { X: 'Y', Y: 'Z', Z: 'X' },
+}
+
+const points2: Record<string, number> = {
+  X: 1,
+  Y: 2,
+  Z: 3,
+}
+
+const points3: Record<string, number> = {
+  X: 0,
+  Y: 3,
+  Z: 6,
+}
+
 solve((lines) => {
   let result = 0
-  lines.forEach((line) => {})
+  lines.forEach((line) => {
+    const chars = line.split('')
+    const them = chars[0]
+    const outcome = chars[2]
+    const me = points[them][outcome]
+
+    const outcomePoints = points3[outcome]
+    const roundTotal = points2[me] + outcomePoints
+    console.log(roundTotal)
+    result = result + roundTotal
+  })
   return result
 })
